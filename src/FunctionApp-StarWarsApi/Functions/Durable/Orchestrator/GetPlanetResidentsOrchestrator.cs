a816e320b7f113ac7dfbd3408e66f618ebbb6510
Callon Campbell <calloncampbell@msn.com>
2024-08-19 15:43:25 -0400

    Added retry policy

--- src/FunctionApp-StarWarsApi/Functions/Durable/Orchestrator/GetPlanetResidentsOrchestrator.cs
+++ src/FunctionApp-StarWarsApi/Functions/Durable/Orchestrator/GetPlanetResidentsOrchestrator.cs
@@ -27,13 +27,20 @@ namespace FunctionApp_StarWarsApi.Functions.Durable.Orchestrator
                 _logger.LogInformation($"{nameof(GetPlanetResidentsOrchestrator)} started for InstanceId: {context.InstanceId}");
             }
 
+            var taskOptions = TaskOptions.FromRetryPolicy(
+                new RetryPolicy(
+                    maxNumberOfAttempts: 3,
+                    firstRetryInterval: TimeSpan.FromSeconds(3),
+                    backoffCoefficient: 1.5,
+                    maxRetryInterval: TimeSpan.FromSeconds(30),
+                    retryTimeout: TimeSpan.FromSeconds(180))
+                );
+
             var planetName = context.GetInput<string>();
 
             var result = new PlanetResidents();
 
-            var planetResult = await context.CallActivityAsync<Planet>(
-                nameof(SearchPlanetActivity),
-                input: planetName);
+            var planetResult = await context.CallActivityAsync<Planet>(nameof(SearchPlanetActivity), input: planetName, taskOptions);
 
             if (planetResult != null)
             {
@@ -42,7 +49,7 @@ namespace FunctionApp_StarWarsApi.Functions.Durable.Orchestrator
                 var tasks = new List<Task<Person>>();
                 foreach (var residentUrl in planetResult.ResidentUrls)
                 {
-                    tasks.Add(context.CallActivityAsync<Person>(nameof(GetCharacterActivity), residentUrl));
+                    tasks.Add(context.CallActivityAsync<Person>(nameof(GetCharacterActivity), residentUrl, taskOptions));
                 }
 
                 await Task.WhenAll(tasks);
